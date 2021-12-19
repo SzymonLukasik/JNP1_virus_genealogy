5e31114adbd4b539b1b8a5473bc66b4127c32a75
Szymon Łukasik <sl428760@students.mimuw.edu.pl>
2021-12-19 20:53:40 +0100

    Added some methods implementation

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -13,59 +13,72 @@ public:
     virtual const char* what() const noexcept {return "VirusAlreadyCreated\n";}
 };
 
-
-class Virus {
-public:
-    using id_type = int;
-};
-
 template<typename Virus>
 class VirusGenealogy {
 private:
+
     struct VirusNode {
         std::shared_ptr<Virus> ptr;
-        std::set<std::shared_ptr<Virus>> kids;
+        std::set<std::shared_ptr<Virus>> childs;
         std::set<std::shared_ptr<Virus>> parents;
 
         VirusNode(Virus::id_type id) : ptr(std::make_shared<Virus>(id)) {}
     };
 
-    std::map<Virus::id_type, std::unique_ptr<VirusNode>> nodes;
+    using nodes_t = std::map<typename Virus::id_type, std::shared_ptr<VirusNode>>;
+    nodes_t nodes;
+    typename Virus::id_type stem_id;
 
 public:
-    using children_iterator = set<std::shared_ptr<Virus>>::iterator;
 
     // Tworzy nową genealogię.
     // Tworzy także węzeł wirusa macierzystego o identyfikatorze stem_id.
     VirusGenealogy(Virus::id_type const &stem_id) : 
-    nodes{{stem_id, std::make_unique<VirusNode>(stem_id)}} {}
+    nodes{{stem_id, std::make_shared<VirusNode>(stem_id)}},
+    stem_id(stem_id) {}
 
     // Zwraca identyfikator wirusa macierzystego.
-    Virus::id_type get_stem_id() const;
+    Virus::id_type get_stem_id() const {return stem_id;}
 
     // Zwraca iterator pozwalający przeglądać listę identyfikatorów
     // bezpośrednich następników wirusa o podanym identyfikatorze.
     // Zgłasza wyjątek VirusNotFound, jeśli dany wirus nie istnieje.
     // Iterator musi spełniać koncept bidirectional_iterator oraz
     // typeid(*v.get_children_begin()) == typeid(const Virus &).
-    VirusGenealogy<Virus>::children_iterator get_children_begin(Virus::id_type const &id) const;
+    //VirusGenealogy<Virus>::children_iterator get_children_begin(Virus::id_type const &id) const;
 
     // Iterator wskazujący na element za końcem wyżej wspomnianej listy.
     // Zgłasza wyjątek VirusNotFound, jeśli dany wirus nie istnieje.
-    VirusGenealogy<Virus>::children_iterator get_children_end(Virus::id_type const &id) const;
+    //VirusGenealogy<Virus>::children_iterator get_children_end(Virus::id_type const &id) const;
 
     // Zwraca listę identyfikatorów bezpośrednich poprzedników wirusa
     // o podanym identyfikatorze.
     // Zgłasza wyjątek VirusNotFound, jeśli dany wirus nie istnieje.
-    std::vector<Virus::id_type> get_parents(Virus::id_type const &id) const;
+    std::vector<typename Virus::id_type> get_parents(Virus::id_type const &id) const {
+        auto it = nodes.find(id);
+        if (it == nodes.end())
+            throw (new VirusNotFound);
+        std::vector<typename Virus::id_type> res;
+        for (auto &it : it->second->parents) {
+            res.push_back(it->get_id());
+        }
+        return res;
+    };
 
     // Sprawdza, czy wirus o podanym identyfikatorze istnieje.
-    bool exists(Virus::id_type const &id) const;
+    bool exists(Virus::id_type const &id) const {
+        return (nodes.find(id) != nodes.end());
+    };
 
     // Zwraca referencję do obiektu reprezentującego wirus o podanym
     // identyfikatorze.
     // Zgłasza wyjątek VirusNotFound, jeśli żądany wirus nie istnieje.
-    const Virus& operator[](Virus::id_type const &id) const;
+    const Virus& operator[](typename Virus::id_type const &id) const {
+        auto it = nodes.find(id);
+        if (it == nodes.end())
+            throw (new VirusNotFound);
+        return *(it->second->ptr);
+    };
 
     // Tworzy węzeł reprezentujący nowy wirus o identyfikatorze id
     // powstały z wirusów o podanym identyfikatorze parent_id lub
@@ -74,19 +87,54 @@ public:
     // id już istnieje.
     // Zgłasza wyjątek VirusNotFound, jeśli któryś z wyspecyfikowanych
     // poprzedników nie istnieje.
-    void create(Virus::id_type const &id, Virus::id_type const &parent_id) {
-        if (nodes.find(id) != nodes.end())
+    void create(typename Virus::id_type const &id, typename Virus::id_type const &parent_id) {
+        typename nodes_t::iterator child_it = nodes.find(id);
+        if (child_it != nodes.end())
             throw (new VirusAlreadyCreated);
-        if (nodes.find(parent_id) != nodes.end())
+        typename nodes_t::iterator parent_it = nodes.find(parent_id);
+        if (parent_it == nodes.end())
             throw (new VirusNotFound);
-        nodes.insert({id, std::make_unique<Virus>(id)});
         
+        VirusNode& child = *nodes.insert({id, std::make_shared<VirusNode>(id)})
+                           .first->second;
+        VirusNode& parent = *parent_it->second;
+        parent.childs.insert(child.ptr);
+        child.parents.insert(parent.ptr);
     };
-    void create(Virus::id_type const &id, std::vector<Virus::id_type> const &parent_ids);
+
+    void create(typename Virus::id_type const &id, std::vector<typename Virus::id_type> const &parent_ids) {
+        typename nodes_t::iterator child_it = nodes.find(id);
+        if (child_it != nodes.end())
+            throw (new VirusAlreadyCreated);
+
+        std::vector<typename nodes_t::iterator> parent_its;
+        for (const typename Virus::id_type &parent_id : parent_ids) {
+            typename nodes_t::iterator parent_it = nodes.find(parent_id);
+            if (parent_it == nodes.end())
+                throw (new VirusNotFound);
+            parent_its.push_back(parent_it);
+        }
+        VirusNode& child = *nodes.insert({id, std::make_shared<VirusNode>(id)})
+                           .first->second;
+        for (typename nodes_t::iterator parent_it : parent_its) {
+            VirusNode& parent = *parent_it->second;
+            parent.childs.insert(child.ptr);
+            child.parents.insert(parent.ptr);
+        }
+    }; 
 
     // Dodaje nową krawędź w grafie genealogii.
     // Zgłasza wyjątek VirusNotFound, jeśli któryś z podanych wirusów nie istnieje.
-    void connect(Virus::id_type const &child_id, virus::id_type const &parent_id);
+    void connect(Virus::id_type const &child_id, Virus::id_type const &parent_id) {
+        typename nodes_t::iterator child_it = nodes.find(child_id),
+                                   parent_it = nodes.find(parent_id);
+        
+        if (child_it == nodes.end() || parent_it == nodes.end())
+            throw (new VirusAlreadyCreated);
+        VirusNode& child = *child_it->second, parent = *parent_it->second;
+        parent.childs.insert(child.ptr);
+        child.parents.insert(parent.ptr);
+    };
 
     // Usuwa wirus o podanym identyfikatorze.
     // Zgłasza wyjątek VirusNotFound, jeśli żądany wirus nie istnieje.
